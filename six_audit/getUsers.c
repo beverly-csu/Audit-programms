8c6a980a47a8bc859b02a180099c8014461d121b
danya_by <d.foteeff@yandex.ru>
2021-03-15 14:35:10 +0500

    sum up

--- six_audit/getUsers.c
+++ six_audit/getUsers.c
@@ -1,1 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "getUsers.h"
+
+void getUserInfo()
+{
+    
+}
--- six_audit/main.c
+++ six_audit/main.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "password.h"
+#include "getUsers.h"
 
 int main(){
-    password();
+    // password();
+    getUserInfo();
 }
