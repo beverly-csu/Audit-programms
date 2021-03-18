fe8d7e2f71f9b5e2d70c0a0c434c9ac34bffbcd2
danya_by <d.foteeff@yandex.ru>
2021-03-18 20:21:00 +0500

    snezhok

--- fifth_audit/colors.h
+++ fifth_audit/colors.h
@@ -4,7 +4,7 @@
 #define RED 31
 #define GREEN 32
 #define YELLOW 33
-#define BLUE 34
+#define BLUE 34 
 #define MAGENTA 35
 #define CYAN 36
 #define WHITE 37
--- fifth_audit/main.c
+++ fifth_audit/main.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <unistd.h>
+#include <windows.h>
 #include "colors.h"
 
-void displayStar(int centerX, int centerY, int color) {
-    printf("\033[%d;%dH*", centerY, centerX);
-    printf("\033[%d;%dH*", centerY - 1, centerX);
-    printf("\033[%d;%dH*", centerY + 1, centerX);
-    printf("\033[%d;%dH*", centerY, centerX - 1);
-    printf("\033[%d;%dH*", centerY, centerX + 1);
+void displayStar(int centerX, int centerY, int color)
+{
+    printf("\033[%dm\033[%d;%dH*\033[0m", color, centerY, centerX);
+    printf("\033[%dm\033[%d;%dH*\033[0m", color, centerY - 1, centerX);
+    printf("\033[%dm\033[%d;%dH*\033[0m", color, centerY + 1, centerX);
+    printf("\033[%dm\033[%d;%dH*\033[0m", color, centerY, centerX - 1);
+    printf("\033[%dm\033[%d;%dH*\033[0m", color, centerY, centerX + 1);
 }
 
 int main()
 {
     srand(time(NULL));
+    int star_data[5][3] = {{5, 5, GREEN}, {9, 4, YELLOW}, {25, 8, RED}, {50, 7, MAGENTA}, {62, 4, CYAN}};
     int chance = 0;
-    printf("\033[2J");
-    displayStar(5, 5, BLACK);
-    // for (int x = 0; x < 24; x++)
-    // {
-    //     printf("\033[2J");
-    //     for (int y = 0; y < 79; y++)
-    //     {
-    //         if ((rand() % 100) % 2 == 0)
-    //             printf("\033[%d;%dH*", x, y);
-    //     }
-    //     sleep(1);
-    // }
-    printf("\033[75;0H");
+    printf("\033[2J\033?25l");
+    while (1)
+    {
+        printf("\033[2J");
+        for (int i = 0; i < 5; i++)
+        {
+            displayStar(star_data[i][0], star_data[i][1], star_data[i][2]);
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            if ((rand() % 10) % 2 == 0 && star_data[i][0] > 0 && star_data[i][0] < 25)
+                star_data[i][0] -= 1;
+            else
+                star_data[i][0] += 1;
+            if ((rand() % 10) % 2 == 0 && star_data[i][1] > 0 && star_data[i][1] < 80)
+                star_data[i][1] -= 1;
+            else
+                star_data[i][1] += 1;
+        }
+        Sleep(1000);
+    }
+    printf("\033[75;0H\033?25h");
 }
