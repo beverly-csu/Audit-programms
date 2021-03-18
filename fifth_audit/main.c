6f3f5431e64518170fcc236c71315966f227a25e
danya_by <d.foteeff@yandex.ru>
2021-03-18 19:11:36 +0500

    Update main.c

--- fifth_audit/main.c
+++ fifth_audit/main.c
@@ -3,20 +3,29 @@
 #include <time.h>
 #include <unistd.h>
 
+void displayStar(int centerX, int centerY, int color) {
+    printf("\033[%d;%dH*", centerY, centerX);
+    printf("\033[%d;%dH*", centerY - 1, centerX);
+    printf("\033[%d;%dH*", centerY + 1, centerX);
+    printf("\033[%d;%dH*", centerY, centerX - 1);
+    printf("\033[%d;%dH*", centerY, centerX + 1);
+}
+
 int main()
 {
     srand(time(NULL));
     int chance = 0;
     printf("\033[2J");
-    for (int x = 0; x < 24; x++)
-    {
-        
-        printf("\033[2J");
-        for (int y = 0; y < 79; y++)
-        {
-            if ((rand() % 100) % 2 == 0)
-                printf("\033[%d;%dH*", x, y);
-        }
-        sleep(1);
-    }
+    displayStar(5, 5, 0);
+    // for (int x = 0; x < 24; x++)
+    // {
+    //     printf("\033[2J");
+    //     for (int y = 0; y < 79; y++)
+    //     {
+    //         if ((rand() % 100) % 2 == 0)
+    //             printf("\033[%d;%dH*", x, y);
+    //     }
+    //     sleep(1);
+    // }
+    printf("\033[75;0H");
 }
