3959ca622e388d72e5d97e98329d9036f4723ea5
danya_by <d.foteeff@yandex.ru>
2021-03-21 20:06:29 +0500

    test

--- six_audit/getUsers.c
+++ six_audit/getUsers.c
@@ -1,8 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "getUsers.h"
+#include "string_lib.h"
+
+char *strParser(char *string, char endChar)
+{
+    char *tempStr = (char *)malloc(sizeof(char));
+    for (int i = 0; i < str_len(string) + 1; i++)
+    {
+        if (string[i] != endChar)
+        {
+            tempStr = (char *)realloc(tempStr, (i + 3) * sizeof(char));
+            tempStr[i] = string[i];
+        }
+        else if (string[i] == endChar)
+        {
+            tempStr[i] = '\0';
+            break;
+        }
+    }
+    string = 0;
+    return tempStr;
+}
+
+int stringsCount(char *filename)
+{
+    FILE *file;
+    file = fopen(filename, "r");
+    char sym = {0};
+    int count = 0;
+    while (1)
+    {
+        sym = fgetc(file);
+        if (sym == '\n')
+            count++;
+        if (sym == EOF)
+            break;
+    }
+    fclose(file);
+    return count + 1;
+}
+
+void delSpaces(char *filename)
+{
+    FILE *file, *newfile;
+    file = fopen(filename, "r");
+    system("touch newfile.txt");
+    newfile = fopen("newfile.txt", "w");
+    char sym = 0;
+    while (1)
+    {
+        sym = fgetc(file);
+        if (sym == ' ')
+        {
+            fputc('_', newfile);
+        }
+        else
+        {
+            fputc(sym, newfile);
+        }
+        if (sym == EOF) {
+            break;
+        }
+    }
+    fclose(file);
+    fclose(newfile);
+}
 
 void getUserInfo()
 {
-    
+    FILE *passwd;
+    char temp[255] = {0};
+    delSpaces("/etc/passwd");
+    passwd = fopen("newfile.txt", "r");
+    *temp = 0;
+    printf("Program found %d local users.\n", stringsCount("/etc/passwd"));
+    int count = stringsCount("/etc/passwd");
+    for (int i = 0; i < count - 1; i++)
+    {
+        fscanf(passwd, "%s", temp);
+        printf("%s\n", strParser(temp, ':'));
+        usleep(300000);
+    }
+    fclose(passwd);
+    system("rm newfile.txt");
 }
